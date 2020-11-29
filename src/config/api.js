0b4ff192b6096b5fa97473f3be3c8060bcb55f69
Agustin Knaupp <agustin.knaupp@southworks.com>
2020-11-29 17:39:48 -0300

    files

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,12 @@
 export const Google_API = {
   url: 'https://www.googleapis.com/customsearch/v1?',
   params: {
-    key: '',
-    cx: '',
+    key: 'AIzaSyCXO78JY5iT71bfgBvTl-XJwic1zUyemz0',
+    cx: '0729ddf5970b977f8',
   },
 };
 
 export const Bing_API = {
   url: 'https://api.bing.microsoft.com/v7.0/search',
-  key: '',
+  key: '74f404a2904c476d83acd8935eef8b25',
 };
